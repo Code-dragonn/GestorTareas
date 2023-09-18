a70f2bf903ee69d0657bae520583f15d913a5ad9
aalejandrozuleta <jhoitan266@gmail.com>
2023-09-17 22:36:28 -0500

    fix: "code documentation"

--- static/js/style.js
+++ static/js/style.js
@@ -1,6 +1,10 @@
 const modal = document.getElementById('modalBox');
 const notesElements = document.querySelectorAll('.notes');
 const notesAdd = document.querySelectorAll('.addNotes');
+const cancelButton = document.querySelector('#btnModalBox');
+//! necessary elements 
+
+//? create modal
 notesElements.forEach((notesElement) => {
     notesElement.addEventListener('click', () => {
         modal.style.display = 'grid';
@@ -12,7 +16,9 @@ notesAdd.forEach((notesAdd) => {
         modal.style.display = 'grid';
     })
 });
-const cancelButton = document.querySelector('#btnModalBox');
+// ! end create modal
+// ? closet modal
 cancelButton.addEventListener('click', () => {
     modal.style.display = 'none';
 });
+// ! closet modal
