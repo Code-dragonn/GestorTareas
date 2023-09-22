c0f5cfc558509c38a2f9773538057e75f9a91837
aalejandrozuleta <jhoitan266@gmail.com>
2023-09-21 19:42:33 -0500

    feature: "Create Task"

--- static/js/style.js
+++ static/js/style.js
@@ -1,24 +1,86 @@
-const modal = document.getElementById('modalBox');
-const notesElements = document.querySelectorAll('.notes');
-const notesAdd = document.querySelectorAll('.addNotes');
-const cancelButton = document.querySelector('#btnModalBox');
-//! necessary elements 
+document.addEventListener("DOMContentLoaded", function () {
+    // DOM Elements
+    const modal = document.getElementById('modalBox');
+    const addNotesButton = document.querySelector('.addNoteButtonToDo');
+    const cancelButton = document.querySelector('#btnModalCancel');
+    const titleInput = document.getElementById("modalTitle");
+    const descriptionInput = document.getElementById("modalDescription");
+    const saveButton = document.getElementById("btnModalSave");
 
-//? create modal
-notesElements.forEach((notesElement) => {
-    notesElement.addEventListener('click', () => {
-        modal.style.display = 'grid';
-    });
-});
+    // Class to create note objects
+    class Note {
+        constructor(title, description) {
+            this.title = title;
+            this.description = description;
+        }
+    }
 
-notesAdd.forEach((notesAdd) => {
-    notesAdd.addEventListener('click',()=>{
+    addNotesButton.addEventListener("click", showModal);
+
+    // Function to display the modal
+    function showModal() {
         modal.style.display = 'grid';
-    })
+    }
+
+    // Function to hide the modal
+    function hideModal() {
+        modal.style.display = 'none';
+    }
+
+    // Function to create a new note and add it to "toDo"
+    function createNote() {
+        const title = titleInput.value;
+        const description = descriptionInput.value;
+
+        if (!title || !description) {
+            alert("Please fill in both the title and description of the note.");
+            return;
+        }
+
+        const newNote = new Note(title, description);
+
+        // Update the graphical interface
+        const newNoteElement = createNoteElement(newNote);
+        const todoSection = document.querySelector('[data-state="to-do"]');
+        todoSection.appendChild(newNoteElement);
+
+        // Clear the modal fields
+        titleInput.value = "";
+        descriptionInput.value = "";
+
+        // Hide the modal after creating the note
+        hideModal();
+    }
+
+    // Function to create a note element in the DOM
+    function createNoteElement(note) {
+        const newNoteElement = document.createElement("article");
+        newNoteElement.classList.add("notes");
+        newNoteElement.innerHTML = `
+            <p>${note.title}</p>
+            <figure class="imgEdit">
+                <img src="static/img/edit.svg" alt="">
+            </figure>
+        `;
+
+        // Add an event listener to show the modal when clicking on the note
+        newNoteElement.addEventListener("click", () => {
+            showNoteInfo(note);
+        });
+
+        return newNoteElement;
+    }
+
+    // Function to display the note information in the modal
+    function showNoteInfo(note) {
+        modal.style.display = 'grid';
+        titleInput.value = note.title;
+        descriptionInput.value = note.description;
+    }
+
+    // Associate the createNote function with the "Save" button in the modal
+    saveButton.addEventListener("click", createNote);
+
+    // Associate the hideModal function with the "Cancel" button in the modal
+    cancelButton.addEventListener("click", hideModal);
 });
-// ! end create modal
-// ? closet modal
-cancelButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-// ! closet modal
