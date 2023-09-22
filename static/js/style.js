fbc20aaf8c0e94535dcf288eb936dc48c7636fca
jhainethv161 <142535660+jhainethv161@users.noreply.github.com>
2023-09-22 01:10:36 -0500

    feature: "feature editTaskDescription"

--- static/js/style.js
+++ static/js/style.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const descriptionInput = document.getElementById("modalDescription");
     const saveButton = document.getElementById("btnModalSave");
 
+    // Global variable for the note being edited
+    let editingNote = null;
+
     // Class to create note objects
     class Note {
         constructor(title, description) {
@@ -25,9 +28,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Function to hide the modal
     function hideModal() {
         modal.style.display = 'none';
+        // Limpiar la nota en edición
+        editingNote = null;
     }
 
-    // Function to create a new note and add it to "toDo"
+    // Function to create a new note or update the editing note
     function createNote() {
         const title = titleInput.value;
         const description = descriptionInput.value;
@@ -37,18 +42,31 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        const newNote = new Note(title, description);
+        if (editingNote) {
+            // If there is a note in edit, update it instead of creating a new one
+            editingNote.title = title;
+            editingNote.description = description;
+            // Update the view of the edited note
+            const noteElement = document.querySelector(`[data-title="${editingNote.title}"]`);
+            if (noteElement) {
+                noteElement.querySelector("p").textContent = editingNote.title;
+            } else {
+                alert ('Accion no valida, intentelo de nuevo.');
+            }
+        } else {
+            const newNote = new Note(title, description);
 
-        // Update the graphical interface
-        const newNoteElement = createNoteElement(newNote);
-        const todoSection = document.querySelector('[data-state="to-do"]');
-        todoSection.appendChild(newNoteElement);
+            // Update the graphical interface
+            const newNoteElement = createNoteElement(newNote);
+            const todoSection = document.querySelector('[data-state="to-do"]');
+            todoSection.appendChild(newNoteElement);
+        }
 
         // Clear the modal fields
         titleInput.value = "";
         descriptionInput.value = "";
 
-        // Hide the modal after creating the note
+        // Hide the modal after creating/editing the note
         hideModal();
     }
 
@@ -56,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function createNoteElement(note) {
         const newNoteElement = document.createElement("article");
         newNoteElement.classList.add("notes");
+        newNoteElement.setAttribute("data-title", note.title);
         newNoteElement.innerHTML = `
             <p>${note.title}</p>
             <figure class="imgEdit">
@@ -66,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Add an event listener to show the modal when clicking on the note
         newNoteElement.addEventListener("click", () => {
             showNoteInfo(note);
-        });
+    });
 
         return newNoteElement;
     }
@@ -76,6 +95,8 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = 'grid';
         titleInput.value = note.title;
         descriptionInput.value = note.description;
+        // Reset stitingNote
+        editingNote = note;
     }
 
     // Associate the createNote function with the "Save" button in the modal
